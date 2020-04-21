116f8de01a976547e6ff9cc62b9dea8873543404
MCP <mcp@macapa.me>
2020-04-21 13:01:03 +0800

    Cleanup of main functionality

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-    const notInSh = ["20170323", "20180323","20190323","20190723", "20200326", "20200328", "20200406", "20200412"];
+    
+    // Array with dates outside of China fixed for testing purposes
+    // This will come from the db
+    const notInSh = ["20170323","20170219", "20180323","20190323","20190723", "20200326", "20200328", "20200406", "20200412", "20151223"];
+
+    
     // Sorting the array to by date
     function sortDates(dates) {
         return dates.sort((a,b) => {
@@ -25,20 +30,21 @@
     let content = "";
     
     for (let i = 5; i > 0; i--) {
-        let currentYear = moment().subtract(i, 'year');
-        let nextYear = moment(currentYear).add(1, 'year');
+        let sortedDates = sortDates(notInSh);
+        let startYear = moment().subtract(i, 'year');
+        let endYear = moment(startYear).add(1, 'year');
+        // calculate the number of days in the year we are looking at to calculate the number of days in China
+        const daysInYear = endYear.diff(startYear, 'days');
+        // counter to keep track of dasy outside of China
         let dayCounter = 0;
-        // console.log(i + " years ago");
-        // console.log(currentYear.format("MMM-DD-YYYY") + " -> " + nextYear.format("MMM-DD-YYYY"));
-        content += "<h2>" + i + " years ago: " + currentYear.format("MMM-DD-YYYY") + " -> " + nextYear.format("MMM-DD-YYYY") + "</h2>";
-        notInSh.forEach(function(date) {
-            if (moment(date).isBetween(currentYear,nextYear)) {
-                content += "<p>" + moment(date).format("MMM-DD-YYYY") + "</p>";
+        content += `<h2>  ${i}  years ago:  ${ startYear.format("MMM-DD-YYYY") }  ->  ${ endYear.format("MMM-DD-YYYY") } (${ daysInYear } days)</h2>`;
+        sortedDates.forEach(function(date) {
+            if (moment(date).isBetween(startYear,endYear)) {
+                content += `<p> ${ moment(date).format("MMM-DD-YYYY") } </p>`;
                 dayCounter++;
             }
         });
-        // console.log("Total for period: " + dayCounter);
-        content += "<h3>Total for period: " + dayCounter + "</h3>";
+        content += `<h3>Not in China: ${ dayCounter } | in China: ${ daysInYear - dayCounter }</h3>`;
     }
     
     $("#elapsed").html(content);
